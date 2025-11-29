b9392299895eb3171d3b1eb8531f6284d4d34041
Іван Потієнко <potik217@gmail.com>
2025-11-30 00:52:47 +0200

    Update version and URLs for mikusays formula

--- Formula/mikusays.rb
+++ Formula/mikusays.rb
@@ -1,26 +1,26 @@
 class Mikusays < Formula
   desc "Like cowsay but with Hatsune Miku"
   homepage "https://github.com/xxanqw/mikusays"
-  version "0.1.4"
+  version "0.1.4-2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.4/mikusays-macos-arm64-0.1.4"
-      sha256 "feeaf1aa7594537db5d4d2c67c57994620bbd8aa93d76d9fbb9b0409c0e96c67"
+      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.4-2/mikusays-macos-arm64-0.1.4-2"
+      sha256 "838b0b704f57c31f4e51bfe4b4699a51224565a136d7b85854a33af9ff86bb7a"
     elsif Hardware::CPU.intel?
-      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.3/mikusays-macos-x64-0.1.4"
-      sha256 "42f02fe70de423ddef788fb8c6e06ddb622b33e9ef5de3e36184ed20bd3f4333"
+      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.4-2/mikusays-macos-x64-0.1.4-2"
+      sha256 "8637a8954e19f0acd4af1e63cf30e3c2d696def3a0901f435191076855c28fcf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.3/mikusays-linux-arm64-0.1.4"
-      sha256 "38da31364e88a6554b1210d043fb00463d09ab37eb75612f0cdfb0f48b2aea28"
+      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.4-2/mikusays-linux-arm64-0.1.4-2"
+      sha256 "651bfdcce5942096df7e5de726d676acb28c4ec54321d390cbb635e5fb6914ca"
     elsif Hardware::CPU.intel?
-      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.3/mikusays-linux-x64-0.1.4"
-      sha256 "12e0670de5a316d17562008a079cb036d7ecc17d34588b47533aceae409860b7"
+      url "https://github.com/xxanqw/mikusays/releases/download/v0.1.4-2/mikusays-linux-x64-0.1.4-2"
+      sha256 "1371a8be0db49c5ea010f1ed928b873fb5d7e16e07631f4ef27e346180c7c3d8"
     end
   end
 
